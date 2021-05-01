0673d1e2a9d7f04f183196142f2bf85bd69ac7e1
Josh Stroud <josh.e.stroud@gmail.com>
2021-05-01 15:49:47 -0700

    Add API route to create messages in a chat

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const bodyParser = require('body-parser')
+
 const chatModels = require('../../models/Chat');
 // const Chat = require('../../models/Chat');
+
+router.use(bodyParser.json());
 
 router.post('/', (req, res) => {
     chatModels.chat.create(req.body)
@@ -20,6 +24,23 @@ router.get('/:chatId', (req, res) => {
     chatModels.chat.findById(req.params.chatId)
         .then(chat => res.json(chat))
         .catch(err => res.status(404).json( {nochatsfound: 'Chat with given id not found'}));
+});
+
+router.post('/:chatId', (req, res) => {
+    const chatId = req.params.chatId;
+    const body = Object.assign(req.body, { chat_id: chatId });
+    chatModels.message.create(body)
+        .then(message => {
+            console.log('Message object:' ,message);
+            chatModels.chat.updateOne( {_id: chatId}, { $push: { messages: message }} )
+                .then(update => {
+                    chatModels.chat.findById(chatId).then(chat => {
+                        console.log('Added message to chat: ', chat);
+                        res.json(chat);
+                    })
+                })
+            })
+        .catch(err => res.status(400).json( { err, error: 'Unable to create message' }));
 });
 
 // router.post('/send', (req, res) => {
