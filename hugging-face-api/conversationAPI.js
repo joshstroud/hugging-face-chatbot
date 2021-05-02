b19c887a8943d6bd2161c63f79e7cad3b74f01f2
Josh Stroud <josh.e.stroud@gmail.com>
2021-05-01 17:04:13 -0700

    Add hugging face conversation api functions

---
+++ hugging-face-api/conversationAPI.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const { response } = require('express');
+
+const API_URL = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large";
+const postHeaders = { "Authorization": "Bearer api_elyYFHzeepHfdyJZftFwngSokNXaPWNrgT" };
+
+// query returns a promise
+function query(payload, callback) {
+    data = JSON.stringify(payload);
+    return axios.post(API_URL, data, { headers: postHeaders });
+}
+
+// Example query use from Conversational Task @ https://api-inference.huggingface.co/docs/python/html/detailed_parameters.html#detailed-parameters
+// query({
+//     "inputs": {
+//         "past_user_inputs": [],
+//         "generated_responses": [],
+//         "text": "Hey, I'm having some mental health problems. Do you have a second to talk?"
+//     },
+// });
+
+function getConversationResponse( { pastUserInputs, generatedResponses, promptText } ) {
+    return query({
+        "inputs": {
+            "past_user_inputs": pastUserInputs,
+            "generated_responses": generatedResponses,
+            "text": promptText
+        },
+    });
+}
+
+module.exports.getConversationResponse = getConversationResponse;
